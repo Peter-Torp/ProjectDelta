0aa1ff4175b4e73e99eec3d21862931048d41ce7
Peter Torp Borggaard <1074147@ucn.dk>
2020-03-04 10:43:10 +0100

    fixed camera + scene loading

---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            print("portal entered");
+
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
