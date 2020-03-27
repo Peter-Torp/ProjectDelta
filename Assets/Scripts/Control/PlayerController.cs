2e2602ea1b8c59d64e49618be8dd05eb639c62c4
Peter-Torp <54837162+Peter-Torp@users.noreply.github.com>
2020-03-27 10:04:46 +0100

    dasda

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -3,6 +3,7 @@ using RPG.Combat;
 using RPG.Movement;
 using RPG.Resources;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.EventSystems;
 
 namespace RPG.Control
@@ -10,7 +11,8 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
         Health health;
-        [SerializeField] private EventSystem events; //LESSON 155
+        [SerializeField] float maxNavMeshProjectionDistance = 1f;
+        [SerializeField] float maxNavPathLength = 40f;
 
 
         [System.Serializable]
@@ -45,9 +47,9 @@ namespace RPG.Control
             SetCursor(CursorType.None);
         }
 
-        private bool InteractWithUI() //PLEASE RECHECK LESSON 155 ON THIS
+        private bool InteractWithUI()
         {
-            if (events.IsPointerOverGameObject(0))
+            if (EventSystem.current.IsPointerOverGameObject())
             {
                 SetCursor(CursorType.UI);
                 return true;
@@ -93,13 +95,13 @@ namespace RPG.Control
 
         private bool InteractWithMovement()
         {
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);  
+            Vector3 target;
+           bool hasHit = RaycastNavMesh(out target); //Raycasting to the navmesh
             if (hasHit)
             {
                 if (Input.GetMouseButton(0))
                 {
-                    GetComponent<Mover>().StartMoveAction(hit.point, 1f);   //move to where the mouse is clicked
+                    GetComponent<Mover>().StartMoveAction(target, 1f);   //move to where the mouse is clicked
                 }
                 SetCursor(CursorType.Movement);
                 
@@ -107,6 +109,42 @@ namespace RPG.Control
                 
             }
             return false;
+        }
+
+        private bool RaycastNavMesh(out Vector3 target)
+        {
+            target = new Vector3();
+            //Raycast to terrain
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (!hasHit) return false;
+            //finding nearest navmesh point
+            NavMeshHit navMeshHit;
+            bool hasCastToNavMesh = NavMesh.SamplePosition(hit.point, out navMeshHit, 
+                maxNavMeshProjectionDistance,
+                NavMesh.AllAreas);
+            if (!hasCastToNavMesh) return false;
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            //return true if found
+            if (!hasPath) return false;
+            //Makes it so we cant walk ontop of roofs
+            if (path.status != NavMeshPathStatus.PathComplete) return false;
+            if (GetPathLength(path) > maxNavPathLength) return false;
+            return true;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float total = 0;
+            if (path.corners.Length < 2) return total;
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                total += Vector3.Distance(path.corners[i], 
+                    path.corners[i + 1]);
+            }
+
+            return 0;
         }
 
         /*Set the cursor to different events*/
